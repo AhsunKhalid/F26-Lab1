6d9aba65cdd4027e7fd0434b26a73dafb4909984
AHSUN KHALID <akhalid63@myseneca.ca>
2026-09-16 19:11:05 +0000

    Completed Lab1a

--- lab1a.py
+++ lab1a.py
@@ -1,8 +1,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: AHSUN KHALID
+# Date: September 16 2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: python3 lab1a.py
 
@@ -10,13 +10,24 @@
 # create a variable called message.
 # Set the variable to equal to "Welcome to PRG101".
 # Print the variable message using print() statement.
+message="Welcome to PRG101"
+print(message)
 
 # TO DO 2: Checking the type of a varibale
 # Use the builtin type() function and print the type of this variable.
+type(message)
 
 # TO DO 3: Dynamic Typing:
 # Create a varibel called `x` and assign it the value 10, then print the type of this variable.
+x=10
+print(type(x))
+
 
 # TO DO 4: Dynamic Typing: 
 # Now reassign a new value to the variable `x`, this value should be a string, e.g "hello", check the type of the variable `x` again.
 # What did you observe?
+x="Python is future"
+print(type(x))
+#I observed that the type of x changed from int tp str due to phython dynamic typing
+"""miltiline 
+comment"""
