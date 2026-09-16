df206813a317c2dba7b0ce4e355a2709585f3fa8
AHSUN KHALID <akhalid63@myseneca.ca>
2026-09-16 20:10:06 +0000

    Completed lab1e

--- lab1b.py
+++ lab1b.py
@@ -8,9 +8,9 @@
 
 # TO-DO 1:
 #	Create a variable called "num1", take its value from user.
-num1=input("Please enter a number")
+num1=input("5")
 #	Create another variable called "num2" and take its value from user.
-num2=input("Please enter another number for num2")
+num2=input("10")
 print(type(num1))
 print(type(num2)) 
 # Convert the values to integers using int() function
@@ -19,4 +19,4 @@ num2=int(num2)
 
 # TO-DO 2:
 # Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
-print("num1 + num2 =, num1+num2")
+print("num1 + num2 =, num1+numb2")
--- lab1c.py
+++ lab1c.py
@@ -9,6 +9,8 @@
 
 #TO-DO 1:
 # import math module.
+import math
 # Create a variable called 'radius' and take its value form user.
 # Convert the variable to integer using int()
 # use the contant pi form math module and compute the area of the circle using the variable 'radius'
+area=math.pi*r*r
